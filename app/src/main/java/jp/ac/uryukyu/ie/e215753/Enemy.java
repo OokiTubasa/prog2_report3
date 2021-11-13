4b5537170351df4ff62a98b8b0714e4b823b0be1
Tsubsa Ooki <e215753@ie.u-ryukyu.ac.jp>
2021-11-13 21:18:45 +0900

    アップデート

--- app/src/main/java/jp/ac/uryukyu/ie/e215753/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215753/Enemy.java
@@ -30,11 +30,15 @@ Heroへ攻撃するメソッド。
 attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
 @param hero 攻撃対象
 */
+
 public void attack(Hero hero){
- int damage = (int)(Math.random() * attack);
- System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
- hero.wounded(damage);
-} /**
+    if( hitPoint > 0 ) {
+        dead = false;
+int damage = (int)(Math.random() * attack);
+System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+hero.wounded(damage);
+}
+}/**
 自身へ攻撃されたときのダメージ処理をするメソッド。
 指定されたダメージを hitPoint から引き、死亡判定を行う。
 @param damage 受けたダメージ */ public void wounded(int damage){ hitPoint -= damage; if( hitPoint < 0 ) {
