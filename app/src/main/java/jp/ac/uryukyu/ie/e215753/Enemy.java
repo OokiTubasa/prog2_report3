09dc0587231aa458187afc00d79378a6e09e0f92
Tsubsa Ooki <e215753@ie.u-ryukyu.ac.jp>
2021-12-03 14:05:49 +0900

    インデント修正

--- app/src/main/java/jp/ac/uryukyu/ie/e215753/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215753/Enemy.java
@@ -13,19 +13,21 @@ public class Enemy {
     private String name;
     private int hitPoint;
     private int attack;
-    private boolean dead;  /**
+    private boolean dead; 
+ /**
 コンストラクタ。名前、最大HP、攻撃力を指定する。
 @param name モンスター名
 @param maximumHP モンスターのHP
 @param attack モンスターの攻撃力
 */
 public Enemy (String name, int maximumHP, int attack) {
- this.name = name;
- hitPoint = maximumHP;
- this.attack = attack;
- dead = false;
- System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
-} /**
+    this.name = name;
+    hitPoint = maximumHP;
+    this.attack = attack;
+    dead = false;
+    System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+} 
+/**
 Heroへ攻撃するメソッド。
 hitpointが０より多い時。つまり、モンスターが生き残っている場合のみ下記のコードを実行。
 attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
@@ -40,13 +42,15 @@ public void attack(Hero hero){
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
         hero.wounded(damage);
     }
-}/**
+}
+/**
 自身へ攻撃されたときのダメージ処理をするメソッド。
 指定されたダメージを hitPoint から引き、死亡判定を行う。
 @param damage 受けたダメージ */ public void wounded(int damage){ hitPoint -= damage; if( hitPoint < 0 ) {
- dead = true;
- System.out.printf("モンスター%sは倒れた。\n", name);
-} }
+    dead = true;
+    System.out.printf("モンスター%sは倒れた。\n", name);
+    } 
+}
 public String getName(){
     return this.name;
 }
